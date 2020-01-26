0f266f7d053f15e0913fd6b0cdc5f50762a4904d
shintaro.sakata <shintaro.sakata.tokyo@gmail.com>
2020-01-26 23:48:28 +0900

    Change entrypoint to be argument

--- src/Compiler.hs
+++ src/Compiler.hs
@@ -1,96 +1,75 @@
 module Compiler where
 
-import           Ports (Port(..), Args(..), ElmType(..))
+import           Ports            (Args (..), ElmType (..), Port (..))
 import           RIO
 import           RIO.List.Partial (head)
-import qualified RIO.Text as T
+import qualified RIO.Text         as T
 
 compile :: [Port] -> FilePath -> RIO SimpleApp ()
-compile ports entryPoint =
-  liftIO $ writeFileUtf8 (entryPoint <> ".d.ts")
-    $ template ports
+compile ports entryPoint = do
+  liftIO $ writeFileUtf8 (entryPoint <> ".d.ts") $ template ports
+  logInfo $ displayShow $ "Success! Generated to " <> (entryPoint <> ".d.ts")
 
 template :: [Port] -> Text
 template ports =
   export $
   buildNameSpace "Elm" $
-    buildNameSpace "Main" $
-      buildInterface "App" "ports: Ports;" <==
-      buildInterface "Args" ("node: HTMLElement;" <> newLine <> "flags: any;") <==
-      buildInterface "Ports" (compilePorts ports) <==
-      buildInterface "Subscribe<T>" "subscribe(callback: (value: T) => any): void;" <==
-      buildInterface "Send<T>" "send(value: T): void;" <==
-      "function init(args: Args): App;"
+  buildNameSpace "Main" $
+  buildInterface "App" "ports: Ports;" <== buildInterface "Args" ("node: HTMLElement;" <> newLine <> "flags: any;") <==
+  buildInterface "Ports" (compilePorts ports) <==
+  buildInterface "Subscribe<T>" "subscribe(callback: (value: T) => any): void;" <==
+  buildInterface "Send<T>" "send(value: T): void;" <==
+  "function init(args: Args): App;"
 
 compilePorts :: [Port] -> Text
 compilePorts []           = ""
 compilePorts (port:ports) = compilePort port <> newLine <> compilePorts ports
 
 compilePort :: Port -> Text
-compilePort port =
-  name port <> ": Subscribe<" <> compileArgs (args port) <> ">"
-  
+compilePort port = name port <> ": Subscribe<" <> compileArgs (args port) <> ">"
+
 compileArgs :: Args -> Text
 compileArgs (Args args) = typeCompatible (head args)
 
 typeCompatible :: ElmType -> Text
 typeCompatible elmType =
   case elmType of
-    Bool_    -> "boolean"
-    Int_     -> "number"
-    Float_   -> "number"
-    String_  -> "string"
-    Any_     -> "any"
-
-
+    Bool_   -> "boolean"
+    Int_    -> "number"
+    Float_  -> "number"
+    String_ -> "string"
+    Any_    -> "any"
 
 export :: Text -> Text
-export content =
-  "export" <-> content <> newLine
-  
+export content = "export" <-> content <> newLine
 
 buildInterface :: Text -> Text -> Text
-buildInterface name prototype =
-  interface <-> name <~
-      prototype
-  ~> empty
-  
+buildInterface name prototype = interface <-> name <~ prototype ~> empty
 
 buildNameSpace :: Text -> Text -> Text
-buildNameSpace name content =
-  namespace <-> name <~
-    content
-  ~> empty
-
+buildNameSpace name content = namespace <-> name <~ content ~> empty
 
 -- RBrace
 (~>) :: Text -> Text -> Text
 (~>) content text = content <> "}" <> text
 
-
 -- LBrace & Indent content
 (<~) :: Text -> Text -> Text
-(<~) text content =
-  text <> " {" <> newLine <--> content
+(<~) text content = text <> " {" <> newLine <--> content
 
 (<==) :: Text -> Text -> Text
 (<==) l r = l <> newLine <> newLine <> r
 
 -- Indent content
 (<-->) :: Text -> Text -> Text
-(<-->) text content =
-  text <> offsideTrap content
-
+(<-->) text content = text <> offsideTrap content
 
 -- "l_text" <-> "r_text" = "l_text r_text"
 (<->) :: Text -> Text -> Text
-(<->) l r =
-  l <> whiteSpace <> r
-
+(<->) l r = l <> whiteSpace <> r
 
 offsideTrap :: Text -> Text
-offsideTrap content =
-  concatLines (map (\line -> whiteSpaces indentSize <> line) (T.lines content))
+offsideTrap content = concatLines (map (\line -> whiteSpaces indentSize <> line) (T.lines content))
 
 concatLines :: [Text] -> Text
 concatLines lines = T.concat $ map (<> newLine) lines
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,15 +2,21 @@ module Lib
   ( libMain
   ) where
 
-import           Compiler     (compile)
-import           Parser       (parser)
+import           Compiler           (compile)
+import           Parser             (parser)
 import           RIO
+import           RIO.List           (headMaybe)
+import           System.Environment (getArgs)
 
 libMain :: IO ()
 libMain =
   runSimpleApp $ do
-    ports <- parser entryPoint
-    compile ports entryPoint
+    ep <- getEntryPoint
+    case ep of
+      Just entryPoint -> do
+        ports <- parser entryPoint
+        compile ports entryPoint
+      Nothing -> logError "Error: Enter elm file entry point"
 
-entryPoint :: FilePath
-entryPoint = "example/project/src/Main.elm"
+getEntryPoint :: RIO SimpleApp (Maybe FilePath)
+getEntryPoint = headMaybe <$> liftIO getArgs
